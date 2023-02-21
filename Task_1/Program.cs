d05d6940e1b24069ce39f30c2805f1dab7c59e8d
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-02-21 07:58:26 +0300

    2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,4 +5,28 @@
 ["Hello", "2", "WORLD"] -> ["2"]
  ["Kazan", "Damask", "Kizil"] -> [] */
 
-
+string[] array1 = new string[4] { "Hello", "2", "WORLD", ":-)" };
+string[] array2 = new string[array1.Length];
+Console.WriteLine($"Результат: ");
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
